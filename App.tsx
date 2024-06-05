3801e7d605e24a93916bf932cd7c82d0a28f31eb
FiqraPutraLesmana <fiqraputralesmana@gmail.com>
2024-06-05 12:33:06 +0700

    first commit

--- App.tsx
+++ App.tsx
@@ -1,117 +1,67 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {useState} from 'react';
+import axios from 'axios';
+import {BASE_URL, API_KEY} from './src/constant';
+import {View, StyleSheet, ActivityIndicator, Text} from 'react-native';
+import WeatherSearch from './src/components/weatherSearch';
+import WeatherInfo from './src/components/weatherInfo';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const App = () => {
+  // Definisikan state "weatherData" dan "setWeatherData"
+  const [weatherData, setWeatherData] = useState();
+  const [status, setStatus] = useState('');
+  const searchWeather = (location: string) => {
+    setStatus('loading');
+    axios
+      .get(`${BASE_URL}?q=${location}&appid=${API_KEY}`)
+      .then(response => {
+        const data = response.data;
+        // Tambahkan code di bawah
+        data.visibility /= 1000;
+        data.visibility = data.visibility.toFixed(2);
+        data.main.temp -= 273.15; // Konversi Kelvin ke Celcius
+        data.main.temp = data.main.temp.toFixed(2);
+        setWeatherData(data);
+        setStatus('success');
+      })
+      .catch(error => {
+        console.log(error);
+        setStatus('error');
+      });
+  };
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const renderComponent = () => {
+    switch (status) {
+      case 'loading':
+        return <ActivityIndicator size="large" />;
+      case 'success':
+        return <WeatherInfo weatherData={weatherData} />;
+      case 'error':
+        return (
+          <Text>
+            Something went wrong. Please try again with a correct city name.
+          </Text>
+        );
+      default:
+        return;
+    }
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <WeatherSearch searchWeather={searchWeather} />
+      {/* Tampilkan data cuaca ketika ada weatherData */}
+      {/* {weatherData && <WeatherInfo weatherData={weatherData} />} */}
+      <View style={styles.margintTop20}>{renderComponent()}</View>
+    </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    padding: 20,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  margintTop20: {
+    marginTop: 20,
   },
 });
 
